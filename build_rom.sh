4f16c362ed6cd6d168d9351e9dec6e60b0d49c1e
P-Salik <peersalik16@gmail.com>
2021-05-10 19:33:11 +0530

    Pixel Experience Build

--- build_rom.sh
+++ build_rom.sh
@@ -5,5 +5,17 @@ set -exv
 # build rom
 . build/envsetup.sh
 lunch aosp_RMX1941-userdebug
+export SKIP_API_CHECKS=true
+export SKIP_ABI_CHECKS=true
+cd /tmp/rom
+export CCACHE_DIR=/tmp/ccache  ##use additional flags if you need(optional)
+export CCACHE_EXEC=$(which ccache)
+export USE_CCACHE=1
+
+ccache -M 30G
+ccache -o compression=true
+ccache -z
+ccache -c
+
 chmod -R 755 out/
 mka bacon -j8
