cba2318ff08ae1db6a303d14d83233d749443ceb
P-Salik <peersalik16@gmail.com>
2021-05-10 21:24:20 +0530

    Pixel Experience Build

--- sync_rom.sh
+++ sync_rom.sh
@@ -9,7 +9,7 @@ repo init -u https://github.com/PixelExperience/manifest -b eleven
 
 git clone https://github.com/P-Salik/local_manifest --depth=1 -b main .repo/local_manifests
 
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j4
 
 # patches
 
